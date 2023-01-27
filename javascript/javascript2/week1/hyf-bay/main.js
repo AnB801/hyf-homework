de9f32e5628050ff45cd0f0b997dc395727abb3b
AnB801 <antony.bondarev@gmail.com>
2023-01-27 13:11:50 +0100

    lil modify

--- javascript/javascript2/week1/hyf-bay/main.js
+++ javascript/javascript2/week1/hyf-bay/main.js
@@ -23,29 +23,4 @@ function renderProducts(products) {
   }
 }
 
-//anoder
-// function renderProducts(products) {
-//   products.forEach((product) => {
-//     const li = document.createElement('li')
-
-//     let shipsToHTML = ''
-//     product.shipsTo.forEach((country) => (shipsToHTML += `<li>${country}</li>`))
-
-//     li.innerHTML = `
-//             <ul>
-//                 <li>${product.name}</li>
-//                 <li>${product.price}</li>
-//                 <li>${product.rating}</li>
-//                 <ul class="ships-to">${shipsToHTML}</ul>
-//             </ul>
-//         `
-//     productsUl.appendChild(li)
-//   })
-// }
-// // 2.2 Searching for products
-// window.onload = function (input) {
-//   products.sort((a, b) => b.rating - a.rating)
-//   renderProducts(products)
-// }
-
 renderProducts(products)
