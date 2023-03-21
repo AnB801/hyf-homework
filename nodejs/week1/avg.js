17ea1c39235bd571d5947f082d49ce7aeb425681
AnB801 <antony.bondarev@gmail.com>
2023-03-21 14:35:31 +0100

    Warm up

--- nodejs/week1/avg.js
+++ nodejs/week1/avg.js
@@ -1,7 +1,7 @@
 const inputNumbers = process.argv.slice(2).map(Number)
 // slice(2), 2 because 0 and 1 index takes service information
 
-//check that the entered number is a number
+//check that the entered number is a number.
 for (let i = 0; i < inputNumbers.length; i++) {
   if (isNaN(inputNumbers[i])) {
     console.log(`${process.argv[i + 2]} is not a number!`)
