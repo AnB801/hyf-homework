5eca197de0fd63ca4cb40836f8a5666bbb0e7eb2
AnB801 <antony.bondarev@gmail.com>
2023-03-28 15:33:02 +0200

    I think these are all the files you need to check my  homework

---
+++ react/react1/week1/Todo_list/src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+
+const App = () => {
+  const todos = [
+    { id: 1, description: 'Get out of bed', when: 'Wed Sep 13 2017' },
+    { id: 2, description: 'Brush teeth', when: 'Thu Sep 14 2017' },
+    { id: 3, description: 'Eat breakfast', when: 'Fri Sep 15 2017' },
+  ]
+
+  return (
+    <div>
+      <h2>Todo List</h2>
+      {todos.map((todo) => (
+        <TodoItem
+          id={todo.id}
+          description={todo.description}
+          when={todo.when}
+        />
+      ))}
+    </div>
+  )
+}
+
+const TodoItem = ({ description, when }) => {
+  return (
+    <div>
+      <p>
+        * {description}, {when}
+      </p>
+    </div>
+  )
+}
+
+export default App
